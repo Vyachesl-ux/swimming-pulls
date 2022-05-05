4a7363a2c91c1fc060e670033f5c1bcf5b39a65a
vyacheslav <vyacheslav@headspin.no>
2022-05-05 20:02:44 +0200

    Add faq files+photos+faq style file +js faq

---
+++ assets/js/faq-accordion.js
@@ -0,0 +1,26 @@
+/**
+ *
+ *  ██████╗ ██████╗ ██╗     ██╗      █████╗ ██████╗ ███████╗    ██╗███╗   ██╗███████╗ ██████╗
+ * ██╔════╝██╔═══██╗██║     ██║     ██╔══██╗██╔══██╗██╔════╝    ██║████╗  ██║██╔════╝██╔═══██╗
+ * ██║     ██║   ██║██║     ██║     ███████║██████╔╝███████╗    ██║██╔██╗ ██║█████╗  ██║   ██║
+ * ██║     ██║   ██║██║     ██║     ██╔══██║██╔═══╝ ╚════██║    ██║██║╚██╗██║██╔══╝  ██║   ██║
+ * ╚██████╗╚██████╔╝███████╗███████╗██║  ██║██║     ███████║    ██║██║ ╚████║██║     ╚██████╔╝
+ *  ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝    ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝
+ *
+ *
+ */
+
+(() => {
+	const accodrions = document.querySelectorAll(".faq-accordion");
+
+	accodrions.forEach(element => {
+		element.body = element.querySelector(".faq-accordion__content-wrapper");
+
+		if(!element.body) return;
+
+		element.addEventListener("click", (e) => {
+			const isOpen = element.classList.toggle("is-open");
+			element.body.style.height = isOpen ? element.body.firstElementChild.offsetHeight + "px" : 0;
+		});
+	});
+})();
