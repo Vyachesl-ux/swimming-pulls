6ca300ae2ac9f546a8327cde3d7b2155868da92e
vyacheslav <vyacheslav@headspin.no>
2022-05-10 19:02:40 +0200

    Add mobile meny

---
+++ assets/js/mobile-navigation.js
@@ -0,0 +1,23 @@
+if (document.documentElement.clientWidth <= 768) {
+   let nav = document.querySelectorAll('.nav-items__link');
+   nav.forEach(e => {
+      e.addEventListener('click', (event) => {
+         if (event.type === 'click') {
+            setTimeout(() => {
+               document.getElementById('menu__toggle').checked = false;
+               document.body.classList.remove('overflow')
+            }, 100);
+         }
+      })
+   })
+}
+let checkbox = document.getElementById('menu__toggle');
+checkbox.addEventListener('click', (e) => {
+   if (checkbox.checked) {
+      document.body.classList.add('overflow')
+   } else {
+      document.body.classList.remove('overflow')
+   }
+     
+});
+
