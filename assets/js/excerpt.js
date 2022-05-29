e88d64e619e83815e28d3339cd4eb0c23c234eb7
Vyacheslav <vyacheslav@headspin.no>
2022-05-29 05:28:05 +0200

    Delete app.js  + add settimeout to the excerpt file

--- assets/js/excerpt.js
+++ assets/js/excerpt.js
@@ -2,37 +2,50 @@ let txt = document.querySelectorAll(".services-content__item-text");
 let btn = document.querySelectorAll(".services-read-more");
 let wrap = document.querySelectorAll(".services-content__item-wrapp");
 
-wrap.forEach(e => {
+wrap.forEach((e) => {
   let elem = e.children[1];
-  
+
   let string = e.children[1].innerHTML;
   console.log(string);
-  if(!(e.classList.contains('show'))){
-    e.children[1].innerHTML = getExcerpt(string, 310).shortText;
+  if (!e.classList.contains("show")) {
+
+    e.children[1].innerHTML = getExcerpt(string, 300).shortText;
   } else {
-    e.children[1].innerHTML = getExcerpt(string, 310).fullText;
+    e.children[1].innerHTML = getExcerpt(string, 300).fullText;
   }
 
-  elem.addEventListener('click', () => {
-      e.classList.toggle('show');
-      if(e.classList.contains('show')) {
-        elem.innerHTML = getExcerpt(string, 310).fullText;
-      } else {
-        elem.innerHTML = getExcerpt(string, 310).shortText;
-      }
-  })
-})
-
+  elem.addEventListener("click", () => {
+    e.classList.toggle("show");
+    if (e.classList.contains("show")) {
+      elem.innerHTML = getExcerpt(string, 300).fullText;
+    } else {
+      setTimeout(() => {
+        elem.innerHTML = getExcerpt(string, 300).shortText;
+      }, 270);
+    }
+  });
+});
 
 function getExcerpt(str, limit) {
   var fullText = str;
   var shortText = str;
   let hash = window.location.hash;
   hash = hash.substring(1);
-  let read = '';
-  hash === 'ru' ? read = "<strong style='text-align: right; display: block;'>Читать далее</strong>" : read = "<strong style='text-align: right; display: block;'>Читати далi</strong>"
-  shortText = shortText.substr(0, shortText.lastIndexOf(" ", limit)) + "..."+"<br>"+read;
-  fullText = fullText + "<br>"+"<strong style='text-align: right; display: block;'>Свернуть</strong>";
+  let read = "";
+  hash === "ru"
+    ? (read =
+        "<strong style='text-align: right; display: block;'>Читать далее</strong>")
+    : (read =
+        "<strong style='text-align: right; display: block;'>Читати далi</strong>");
+  shortText =
+    shortText.substr(0, shortText.lastIndexOf(" ", limit)) +
+    "..." +
+    "<br>" +
+    read;
+  fullText =
+    fullText +
+    "<br>" +
+    "<strong style='text-align: right; display: block;'>Свернуть</strong>";
   var returnString = {
     fullText: fullText,
     shortText: shortText,
