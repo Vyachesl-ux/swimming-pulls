799318c8a7b24d84b480ca3fda02d53a0b5a4325
vyacheslav <vyacheslav@headspin.no>
2022-05-30 14:13:28 +0200

    Edit section services

--- assets/js/excerpt.js
+++ assets/js/excerpt.js
@@ -1,60 +1,79 @@
-let txt = document.querySelectorAll(".services-content__item-text");
-let btn = document.querySelectorAll(".services-read-more");
+(() => {
 let wrap = document.querySelectorAll(".services-content__item-wrapp");
 
-wrap.forEach((e) => {
-  let elem = e.children[1];
-
-  let string = e.children[1].innerHTML;
-  console.log(string);
-  if (!e.classList.contains("show")) {
-    e.children[1].innerHTML = getExcerpt(string, 300).shortText;
-  } else {
-    e.children[1].innerHTML = getExcerpt(string, 300).fullText;
-  }
-
-  elem.addEventListener("click", () => {
-    e.classList.toggle("show");
-    if (e.classList.contains("show")) {
-      elem.innerHTML = getExcerpt(string, 300).fullText;
+  wrap.forEach((e) => {
+    let elem = e.children[1];
+  
+    let string = e.children[1].innerHTML;
+    console.log(string);
+    if (!e.classList.contains("show")) {
+      e.children[1].innerHTML = getExcerpt(string, 300).shortText;
     } else {
-      setTimeout(() => {
-        elem.innerHTML = getExcerpt(string, 300).shortText;
-      }, 270);
+      e.children[1].innerHTML = getExcerpt(string, 300).fullText;
     }
+    if(document.documentElement.clientWidth <= 320){
+  
+      if (!e.classList.contains("show")) {
+        e.children[1].innerHTML = getExcerpt(string, 150).shortText;
+      } else {
+        e.children[1].innerHTML = getExcerpt(string, 150).fullText;
+      }
+    }
+  
+    elem.addEventListener("click", () => {
+      e.classList.toggle("show");
+      if (e.classList.contains("show")) {
+        elem.innerHTML = getExcerpt(string, 300).fullText;
+      } else {
+        setTimeout(() => {
+          elem.innerHTML = getExcerpt(string, 300).shortText;
+        }, 510);
+      }
+      if(document.documentElement.clientWidth <= 320){
+        
+        if (e.classList.contains("show")) {
+          elem.innerHTML = getExcerpt(string, 150).fullText;
+        } else {
+          setTimeout(() => {
+            elem.innerHTML = getExcerpt(string, 150).shortText;
+          }, 510);
+        }
+      }
+    });
   });
-});
-
-function getExcerpt(str, limit) {
-  var fullText = str;
-  var shortText = str;
-  let hash = window.location.hash;
-  hash = hash.substring(1);
-  let read = "";
-  hash === "ru"
+  
+  function getExcerpt(str, limit) {
+    var fullText = str;
+    var shortText = str;
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    let read = "";
+    hash === "ru"
     ? (read =
-        "<strong style='text-align: right; display: block;'>Читать далее</strong>")
-    : (read =
+      "<strong style='text-align: right; display: block;'>Читать далее</strong>")
+      : (read =
         "<strong style='text-align: right; display: block;'>Читати далi</strong>");
-  shortText =
-    shortText.substr(0, shortText.lastIndexOf(" ", limit)) +
-    "..." +
-    "<br>" +
-    read;
-  if (hash === "ru") {
-    fullText =
-      fullText +
+        shortText =
+        shortText.substr(0, shortText.lastIndexOf(" ", limit)) +
+        "..." +
+        "<br>" +
+        read;
+        if (hash === "ru") {
+          fullText =
+          fullText +
       "<br>" +
       "<strong style='text-align: right; display: block;'>Свернуть</strong>";
-  } else {
-    fullText =
+    } else {
+      fullText =
       fullText +
       "<br>" +
       "<strong style='text-align: right; display: block;'>Згорнути</strong>";
+    }
+    var returnString = {
+      fullText: fullText,
+      shortText: shortText,
+    };
+    return returnString;
   }
-  var returnString = {
-    fullText: fullText,
-    shortText: shortText,
-  };
-  return returnString;
-}
+  
+})()
