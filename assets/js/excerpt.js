b41f4be66e17ce0ba582fa852c3fd35595b5a4f9
Vyacheslav <vyacheslav@headspin.no>
2022-06-01 20:37:42 +0200

    Remove excerption from the desktop

--- assets/js/excerpt.js
+++ assets/js/excerpt.js
@@ -1,35 +1,39 @@
 (() => {
-let wrap = document.querySelectorAll(".services-content__item-wrapp");
+  let wrap = document.querySelectorAll(".services-content__item-wrapp");
 
   wrap.forEach((e) => {
     let elem = e.children[1];
-  
+
     let string = e.children[1].innerHTML;
-    if (!e.classList.contains("show")) {
-      e.children[1].innerHTML = getExcerpt(string, 300).shortText;
-    } else {
-      e.children[1].innerHTML = getExcerpt(string, 300).fullText;
+    if((document.documentElement.clientWidth <= 600)){
+      if (!e.classList.contains("show")) {
+        e.children[1].innerHTML = getExcerpt(string, 300).shortText;
+      } else {
+        e.children[1].innerHTML = getExcerpt(string, 300).fullText;
+      }
+
     }
-    if(document.documentElement.clientWidth <= 320){
-  
+    if (document.documentElement.clientWidth <= 320) {
       if (!e.classList.contains("show")) {
         e.children[1].innerHTML = getExcerpt(string, 150).shortText;
       } else {
         e.children[1].innerHTML = getExcerpt(string, 150).fullText;
       }
     }
-  
-    elem.addEventListener("click", () => {
-      e.classList.toggle("show");
-      if (e.classList.contains("show")) {
-        elem.innerHTML = getExcerpt(string, 300).fullText;
-      } else {
-        setTimeout(() => {
-          elem.innerHTML = getExcerpt(string, 300).shortText;
-        }, 510);
-      }
-      if(document.documentElement.clientWidth <= 320){
-        
+    if(document.documentElement.clientWidth <= 600){
+      elem.addEventListener("click", () => {
+        e.classList.toggle("show");
+        if (e.classList.contains("show")) {
+          elem.innerHTML = getExcerpt(string, 300).fullText;
+        } else {
+          setTimeout(() => {
+            elem.innerHTML = getExcerpt(string, 300).shortText;
+          }, 510);
+        }
+      });
+    }
+    if (document.documentElement.clientWidth <= 320) {
+      elem.addEventListener('click', () => {
         if (e.classList.contains("show")) {
           elem.innerHTML = getExcerpt(string, 150).fullText;
         } else {
@@ -37,10 +41,11 @@ let wrap = document.querySelectorAll(".services-content__item-wrapp");
             elem.innerHTML = getExcerpt(string, 150).shortText;
           }, 510);
         }
-      }
-    });
+      })
+    }
+      
   });
-  
+
   function getExcerpt(str, limit) {
     var fullText = str;
     var shortText = str;
@@ -48,25 +53,25 @@ let wrap = document.querySelectorAll(".services-content__item-wrapp");
     hash = hash.substring(1);
     let read = "";
     hash === "ru"
-    ? (read =
-      "<strong style='text-align: right; display: block;'>Читать далее</strong>")
+      ? (read =
+          "<strong style='text-align: right; display: block;'>Читать далее</strong>")
       : (read =
-        "<strong style='text-align: right; display: block;'>Читати далi</strong>");
-        shortText =
-        shortText.substr(0, shortText.lastIndexOf(" ", limit)) +
-        "..." +
-        "<br>" +
-        read;
-        if (hash === "ru") {
-          fullText =
-          fullText +
+          "<strong style='text-align: right; display: block;'>Читати далi</strong>");
+    shortText =
+      shortText.substr(0, shortText.lastIndexOf(" ", limit)) +
+      "..." +
       "<br>" +
-      "<strong style='text-align: right; display: block;'>Свернуть</strong>";
+      read;
+    if (hash === "ru") {
+      fullText =
+        fullText +
+        "<br>" +
+        "<strong style='text-align: right; display: block;'>Свернуть</strong>";
     } else {
       fullText =
-      fullText +
-      "<br>" +
-      "<strong style='text-align: right; display: block;'>Згорнути</strong>";
+        fullText +
+        "<br>" +
+        "<strong style='text-align: right; display: block;'>Згорнути</strong>";
     }
     var returnString = {
       fullText: fullText,
@@ -74,5 +79,4 @@ let wrap = document.querySelectorAll(".services-content__item-wrapp");
     };
     return returnString;
   }
-  
-})()
+})();
